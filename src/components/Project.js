85556eee7f011919e36f872bc0334d17754d0d79
thenatural86 <philip.smalls86@gmail.com>
2021-02-02 20:20:08 -0500

    project details

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,6 +16,24 @@ const Project = ({
   return (
     <article className="project">
       <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      <div className="project-info">
+        <span className="project-number">0{index + 1}</span>
+        <h3>{title}</h3>
+        <p className="project-desc">{description}</p>
+        <div className="project-stack">
+          {stack.map(item => {
+            return <span key={item.id}>{item.title}</span>
+          })}
+        </div>
+        <div className="project-links">
+          <a target="_blank" href={github}>
+            <FaGithubSquare className="project-icon" />
+          </a>
+          <a target="_blank" href={url}>
+            <FaShareSquare className="project-icon" />
+          </a>
+        </div>
+      </div>
     </article>
   )
 }
