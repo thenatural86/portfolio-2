ecf6658c9a70b57d672d7b4f4bcc22639f8507fd
thenatural86 <philip.smalls86@gmail.com>
2021-02-04 19:34:59 -0500

    change

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
+import { Link } from "gatsby"
 
 const Blog = ({
   data: {
@@ -13,6 +14,9 @@ const Blog = ({
     <Layout>
       <section className="blog-page">
         <Blogs blogs={blogs} title="blogs" />
+        <Link to="/" className="btn center-btn" style={{ marginTop: "-2rem" }}>
+          Home
+        </Link>
       </section>
     </Layout>
   )
